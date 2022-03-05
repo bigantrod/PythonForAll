b68a96f01aa1023ee16b76a69fb4a2576821aae3
Anthony Rodriguez <bigantrod@gmail.com>
2022-03-04 19:41:02 -0500

    Chapter 3 inital commit

---
+++ Chapter3/c3e1.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number:"))
+
+if num == 0:
+    print("The number you entered was zero")
+elif num > 0:
+    print("The number you entered was positive")
+elif num < 0:
+    print("The number you entered was negative")
